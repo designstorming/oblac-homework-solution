67a934e6e0512256f9131d067dfc35ebe40daef9
designstorming <designstorming@users.github.com>
2023-09-06 17:03:40 +0200

    feat(app): create app and integrate the library

--- projects/homework-app/src/app/app.component.ts
+++ projects/homework-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Device, DeviceParameter } from '@homework-lib/models';
+import { DevicesFacade } from '@homework-lib/store/devices';
+import { ParametersFacade } from '@homework-lib/store/parameters';
+import { buildLabel } from '@homework-lib/utils';
 
 @Component({
   selector: 'hw-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'homework-app';
+export class AppComponent implements OnInit {
+
+  protected devices$ = this.devicesFacade.devices$;
+  protected parameters$ = this.parametersFacade.parameters$;
+  protected parametersLoading$ = this.parametersFacade.isLoading$;
+
+  constructor(
+    private devicesFacade: DevicesFacade,
+    private parametersFacade: ParametersFacade,
+  ) {}
+
+  ngOnInit(): void {
+    this.devicesFacade.fetchDevices();
+  }
+
+  protected handleActivate(id: Device['id']) {
+    this.parametersFacade.fetchParameters(id);
+  }
+
+  protected handleUpdate({id, value}: {id: DeviceParameter['id'], value: DeviceParameter['value']}) {
+    this.parametersFacade.updateParamter(id, value);
+  }
+
+  protected trackByParamId(index: number, param: DeviceParameter) {
+    return buildLabel(param);
+  }
 }
--- projects/homework-app/src/app/app.module.ts
+++ projects/homework-app/src/app/app.module.ts
@@ -1,9 +1,19 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { DeviceComponent } from '@homework-lib/components/device';
+import { DeviceParameterComponent } from '@homework-lib/components/device-parameter';
+import { ContentTypeInterceptor, HttpStatusInterceptor } from '@homework-lib/interceptors';
+import { DevicesFacade, ParametersFacade, appEffects, appStore } from '@homework-lib/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -12,9 +22,34 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    StoreModule.forRoot(appStore),
+    EffectsModule.forRoot(appEffects),
+    ReactiveFormsModule,
+    HttpClientModule,
+    DeviceParameterComponent,
+    DeviceComponent,
+    ScrollingModule
   ],
-  providers: [],
+  providers: [{
+      provide: HTTP_INTERCEPTORS,
+      useClass: ContentTypeInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpStatusInterceptor,
+      multi: true
+    },
+    DevicesFacade,
+    ParametersFacade,
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
